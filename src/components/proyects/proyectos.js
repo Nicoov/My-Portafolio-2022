8eb237295a4120fac30c664a0b569e9415e43591
Nicoov <nicolas_ignacio_1@hotmail.com>
2023-03-01 14:17:01 -0300

    update v2

--- src/components/proyects/proyectos.js
+++ src/components/proyects/proyectos.js
@@ -4,6 +4,7 @@ import tenisup from '../../photos/TenisUp.jpeg';
 import discord from '../../photos/Discord.png'
 import quiz from '../../photos/Quiz.jpg'
 import mercado from '../../photos/Super.png'
+import anime from '../../photos/animelist.jpg'
 import { useTranslation } from 'react-i18next';
 
 
@@ -71,6 +72,21 @@ const Proyectos = () => {
                         <p> JavaScript - React</p>
                         <div className='link'>
                             <a href='https://github.com/Nicoov/quiz-aplication' rel="noreferrer" target='_blank'>
+                                <button>{t("view-3")}</button>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+                <div className='card'>
+                    <div className='face front'>
+                        <img className="front-img" alt='proyecto1' src={anime}></img>
+                    </div>
+                    <div className='face back'>
+                        <p>2023</p>
+                        <p> {t('about-pro-5')}</p>
+                        <p> JavaScript - React - Jikan Api</p>
+                        <div className='link'>
+                            <a href='https://github.com/Nicoov/animelist' rel="noreferrer" target='_blank'>
                                 <button>{t("view-3")}</button>
                             </a>
                         </div>
